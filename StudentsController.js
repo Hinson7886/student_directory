9e91faa13dd323f7983455ece68141966bc19b3f
Your Actual Name <Your Actual Email>
2015-11-30 01:47:58 -0500

    first commit

---
+++ StudentsController.js
@@ -0,0 +1,12 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+	$scope.students = [
+		{ name: "Sam Schmidt"},
+		{ name: "Jessica Martin"},
+		{ name: "Sandy Smith"},
+		{ name: "Ryan Samuels"},
+		{ name: "Brentwood Davis"}
+
+
+
+	]
+})
